e0d63ef05066843bd34a8f26060a77f58ed71e95
andyMonterrubio <andy.mont18@gmail.com>
2020-05-13 12:22:43 -0500

    adding home and not found pages

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -8,7 +8,18 @@ import {
     CLEAR_USERS,
     GET_USER,
     GET_REPOS
-} from '../types'
+} from '../types';
+
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production'){
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = props => {
     const initialState = {
@@ -25,8 +36,7 @@ const GithubState = props => {
         setLoading();
     
         const res =  await axios.get(`https://api.github.com/search/users?q=${user}&client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID}
-        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+            githubClientId}&client_secret=${githubClientSecret}`);
         
         dispatch({
             type: SEARCH_USERS,
@@ -39,8 +49,7 @@ const GithubState = props => {
         setLoading();
     
         const res =  await axios.get(`https://api.github.com/users/${username}?client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID}
-        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+            githubClientId}&client_secret=${githubClientSecret}`);
         
         dispatch({
             type: GET_USER,
@@ -53,8 +62,8 @@ const GithubState = props => {
         setLoading();
     
         const res =  await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc
-        &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        &client_id=${githubClientId}
+        &client_secret=${githubClientSecret}`);
     
         dispatch({
             type: GET_REPOS,
